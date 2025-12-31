0c6d8498281f4bce919706017a4d91cee5719a4c
JusTechRun Systems <justechrun@gmail.com>
2025-12-31 15:35:47 +0000

    Try fixing this error: `Build Error: Export useFunctions doesn't exist i

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,6 +4,7 @@ import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'
+import { getFunctions } from 'firebase/functions';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
@@ -36,15 +37,18 @@ export function getSdks(firebaseApp: FirebaseApp) {
   return {
     firebaseApp,
     auth: getAuth(firebaseApp),
-    firestore: getFirestore(firebaseApp)
+    firestore: getFirestore(firebaseApp),
+    functions: getFunctions(firebaseApp)
   };
 }
 
 export * from './provider';
 export * from './client-provider';
+export * from './auth/use-user';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
 export * from './non-blocking-updates';
 export * from './non-blocking-login';
 export * from './errors';
 export * from './error-emitter';
+
