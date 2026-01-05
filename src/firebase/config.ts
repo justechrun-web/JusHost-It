5cfa4c25d3f63736982c43bf87a11bd500413618
JusTechRun Systems <justechrun@gmail.com>
2026-01-05 20:09:09 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9278770297-83c54",
   "appId": "1:454472165854:web:89a4c4e88f2bd24715d54f",
-  "apiKey": "AIzaSyDfeErj8vVU0DoATo8i70zCJ0t5YZMW4Bk",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-9278770297-83c54.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "454472165854"
